111ce26701408081f48f69d56aa178fc5fde920a
Tyler James Gunter <Tyler.james.gunter@gmail.com>
2020-10-31 12:22:50 -0400

    complete Isearch function

--- Isearch.cpp
+++ Isearch.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include <string>
 #include <fstream>
-
+#include <string>
 using namespace std;
 
 int main (){
@@ -11,8 +11,22 @@ int main (){
 	'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
 	'x', 'y', 'z'};
 
-	cout << "Alphabet contains: " << Alphabet[1] << endl; 
+	string user_word = "";
+	int i = 0;
 
-	return 0;
-
+	while(i <= 29){
+		char user_input;
+		cout << "Are you thinking of the following character? '" << Alphabet[i] << "' (y/n)\n" << endl;
+		cin >> user_input;
+		if ((user_input == 'y') && (Alphabet[i] != '!')){
+			user_word += Alphabet[i];
+			i = 0;
+		} else if (user_input == 'n'){
+			i += 1;
+		} else{
+			break;
+		}
+	}
+	cout << "You typed:\n" << user_word << endl;
+return 0;
 }
